3b718a07dfc946b5e0bb5314e1b69b371d9ae9fb
cmoonth <cmoonth47@gmail.com>
2024-05-19 15:37:33 +0900

    generic package - update WildcardEx - update WildcardMain1 - add WildcardMain2

--- src/generic/ex5/WildcardEx.java
+++ src/generic/ex5/WildcardEx.java
@@ -5,6 +5,7 @@ import generic.animal.Animal;
 public class WildcardEx {
 
     static <T> void printGenericV1(Box<T> box) {
+//        T t = box.get();      //T의 타입을 반환
         System.out.println("T = " + box.get());
     }
 
@@ -24,12 +25,13 @@ public class WildcardEx {
     //와일드 카드를 이용하면 Box<?>에 Box<Dog>, Box<Cat>, Box<Object> 등 어떠한 타입이든지 들어올 수 있음
     //?만 이용해서 모든 타입을 받을 수 있는 와일드 카드를 비제한 와일드 카드라고 함.
     static void printWildcardV1(Box<?> box) {
+//        Object object = box.get();  //Object 타입을 반환
         System.out.println("? = " + box.get());
     }
 
     static void printWildcardV2(Box<? extends Animal> box) {
-        Animal animal = box.get();
-        System.out.println("이름 = " + animal.getName());
+            Animal animal = box.get();
+            System.out.println("이름 = " + animal.getName());
     }
 
     static Animal printAndReturnWildcard(Box<? extends Animal> box) {
--- src/generic/ex5/WildcardMain1.java
+++ src/generic/ex5/WildcardMain1.java
@@ -18,6 +18,8 @@ public class WildcardMain1 {
         WildcardEx.printGenericV2(dogBox);
         WildcardEx.printWildcardV2(dogBox);
 
+        //와일드 카드를 사용해서 매개변수로써 제네릭 타입을 받아서 사용하는 것을 권장하지만,
+        //특정 타입으로 제한을 두는 경우 타입을 반환받을 때 타입 매개변수가 필요하므로 제네릭 타입이나 제네릭 메서드를 사용
         Dog dog = WildcardEx.printAndReturnGeneric(dogBox);
         Animal animal = WildcardEx.printAndReturnWildcard(dogBox);
 
