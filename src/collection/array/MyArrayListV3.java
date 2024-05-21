7ea8850a8aa0fc1ec8b002ae81b98b499fecb465
cmoonth <cmoonth47@gmail.com>
2024-05-21 14:30:15 +0900

    collection package array - update MyArrayListV2 - add MyArrayListV3 - add MyArrayListV3Main

--- src/collection/array/MyArrayListV3.java
+++ src/collection/array/MyArrayListV3.java
@@ -2,18 +2,18 @@ package collection.array;
 
 import java.util.Arrays;
 
-public class MyArrayListV2 {
+public class MyArrayListV3 {
 
     private static final int DEFAULT_CAPACITY = 5;
 
     private Object[] elementData;
     private int size = 0;
 
-    public MyArrayListV2() {
+    public MyArrayListV3() {
         elementData = new Object[DEFAULT_CAPACITY];
     }
 
-    public MyArrayListV2(int initialCapacity) {
+    public MyArrayListV3(int initialCapacity) {
         elementData = new Object[initialCapacity];
     }
 
@@ -30,6 +30,24 @@ public class MyArrayListV2 {
         //데이터를 마지막에 추가
         elementData[size] = e;
         size++;
+    }
+
+    //코드 추가
+    public void add(int index, Object e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        //데이터 이동
+        shiftRightFrom(index);
+        elementData[index] = e;
+        size++;
+    }
+
+    //요소의 마지막 부터 index까지 데이터 밀기
+    private void shiftRightFrom(int index) {
+        for (int i = size; i > index; i--) {
+            elementData[i] = elementData[i-1];
+        }
     }
 
     //코드 추가
@@ -58,6 +76,25 @@ public class MyArrayListV2 {
         Object oldValue = get(index);
         elementData[index] = element;       //값을 교체
         return oldValue;        //기존의 값 반환
+    }
+
+    //코드 추가
+    public Object remove(int index) {
+        Object oldValue = get(index);
+        //데이터 이동
+        shiftLeftFrom(index);
+
+        size--;
+        elementData[size] = null;
+        return oldValue;
+    }
+
+    //코드 추가
+    //요소에 index부터 마지막까지 왼쪽으로 밀기
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i <size - 1; i++) {
+            elementData[i] = elementData[i+1];
+        }
     }
 
     // 인덱스 조회
